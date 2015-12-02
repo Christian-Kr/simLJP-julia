0902ced02bfde0d7d16a874d9c388abaed76c4f4
Christian Krippendorf <Coding@Christian-Krippendorf.de>
2015-12-02 06:48:14 +0100

    Remove type

--- simLJP.jl
+++ simLJP.jl
@@ -5,47 +5,43 @@ using PyCall
 
 using Distributions
 
-type SimulationSystem
-    steps = 2000
-    particles = 64
-    size = 3.0e-10
-    dim = 2
-end
+steps = 2000
+particles = 64
+size = 3.0e-10
+dim = 2
 
-type Atom
-    diameter = 2.6e-10
-    mass = 6.69e-26
-end
+diameter = 2.6e-10
+mass = 6.69e-26
 
-type LJP
-    sigma = 3.4e-10
-    epsilon = 1.65e-21
-end
+sigma = 3.4e-10
+epsilon = 1.65e-21
 
-type Results
-    positons = []
-    temperatures = []
-end
+positions = []
+temperatures = []
 
 function ljpCalc(positionA, positionB)
+    global sigma, epsilon
+    
     r = norm(positionA - positionB)
-    force = 24 * LJP.epsilon * (2 * (LJP.sigma^12 / r^13) - (LJP.sigma^6 / r^7))
+    force = 24 * epsilon * (2 * (sigma^12 / r^13) - (sigma^6 / r^7))
     return force * (positionA - positionB)
 end
 
 function adjustPosition(position, velocity, closedBox = true)
+    global size
+    
     if closedBox == false
         for i = 1 : length(position)
-            if position[i] > SimulationSystem.size
-                position[i] = -SimulationSystem.size
-            elseif position[i] < -SimulationSystem.size
-                position[i] = SimulationSystem.size
+            if position[i] > size
+                position[i] = -size
+            elseif position[i] < -size
+                position[i] = size
             end    
         end
     else
         for i = 1 : length(position)
-            if position[i] > SimulationSystem.size ||
-               position[i] < -SimulationSystem.size
+            if position[i] > size ||
+               position[i] < -size
                 velocity[i] *= -1
             end
         end     
@@ -54,6 +50,8 @@ function adjustPosition(position, velocity, closedBox = true)
 end
 
 function temperature(mass, velocities)
+    global particles
+    
     T = 0.0
     kB = 1.38064852e-23
 
@@ -63,53 +61,56 @@ function temperature(mass, velocities)
             sum += velocityCoord^2
         end
     end
-    return sum * mass / (kB * 3 * (SimulationSystem.particles - 1))
+    return sum * mass / (kB * 3 * (particles - 1))
 end
 
 function simulation()
     println("func:simulation()")
 
-    velocity = sqrt(LJP.epsilon / Atom.mass)
-    timeStep = 0.0001 * ljpSigma * sqrt(Atom.mass / LJP.epsilon)
+    global temperatures, positions, mass, epsilon, sigma, timeStep, size,
+           particles, steps
+
+    velocity = sqrt(epsilon / mass)
+    timeStep = 0.0001 * sigma * sqrt(mass / epsilon)
     timeStep2 = timeStep^2
 
-    positions = [[0.0 for i in 1:SimulationSystem.dim]
-                 for j in 1:SimulationSystem.particles]
-    numPartSide = 64^(1 / SimulationSystem.dim)
-    distance = (2 * glSimBoxSize) / (numPartSide + 1)
-    for i = 1 : SimulationSystem.particles
+    positions = [[0.0 for i in 1:dim]
+                 for j in 1:particles]
+    numPartSide = 64^(1 / dim)
+    distance = (2 * size) / (numPartSide + 1)
+    for i = 1 : particles
         multX = divrem(i, numPartSide)[2]
         multY = divrem(i - 1, numPartSide)[1]
 
         if multX == 0 multX = numPartSide end
 
-        x = -SimulationSystem.size + distance * multX
-        y = -SimulationSystem.size + distance * (multY + 1)
+        x = -size + distance * multX
+        y = -size + distance * (multY + 1)
         positions[i] = [x, y]
     end
     
     velocities = [rand(Uniform(-1, 1)) * [1.0, 1.0] * velocity
-                  for i in 1:SimulationSystem.particles]
-    accelerations = [[0.0, 0.0] for i in 1:SimulationSystem.particles]
-    forces = [[0.0, 0.0] for i in 1:SimulationSystem.particles]
+                  for i in 1:particles]
+    accelerations = [[0.0, 0.0] for i in 1:particles]
+    forces = [[0.0, 0.0] for i in 1:particles]
 
-    Results.positions = [collect(positions) for i in 1:SimulationSystem.steps]
+    positions = [collect(positions) for i in 1:steps]
 
     # Die Energie des Systems für jeden Zeitschritt.
-    finalEnergies = [0.0 for i in 1:SimulationSystem.steps]
+    finalEnergies = [0.0 for i in 1:steps]
 
     # Die Temperaturen des Systems für jeden Zeitschritt.
-    Results.temperatures = [0.0 for i in 1:SimulationSystem.steps]
+    temperatures = [0.0 for i in 1:steps]
     
     # Starten der Hauptschleife für die Berechnung der neuen Positionen.
-    for i = 2 : SimulationSystem.steps
-        for j = 1 : SimulationSystem.particles
-            position = Results.positions[i - 1][j]
+    for i = 2 : steps
+        for j = 1 : particles
+            position = positions[i - 1][j]
 
             # Kräfte aktualisieren...
             force = [0, 0]
-            for k = j + 1 : SimulationSystem.particles 
-                force = ljpCalc(position, Results.positions[i - 1][k])
+            for k = j + 1 : particles 
+                force = ljpCalc(position, positions[i - 1][k])
                 forces[j] += force
                 forces[k] += -force
             end
@@ -117,24 +118,25 @@ function simulation()
             # Positionen aktualisieren
             newPosition = position + velocities[j] * timeStep + 0.5 *
                            accelerations[j] * timeStep2
-            Results.positions[i][j] = adjustPosition(newPosition, velocities[j])
+            positions[i][j] = adjustPosition(newPosition, velocities[j])
 
             # Beschleunigungen aktualisieren
-            accelerations[j] = forces[j] / Atom.mass
+            accelerations[j] = forces[j] / mass
 
             # Geschwindigkeit aktualisieren
             velocities[j] = velocities[j] + accelerations[j] * timeStep
         end
         # Berechne und speicher die Temperatur für den Schritt.
-        Results.temperatures[i] = temperature(Atom.mass, velocities)
-        forces = [[0.0, 0.0] for i in 1:SimulationSystem.particles]
+        temperatures[i] = temperature(mass, velocities)
+        forces = [[0.0, 0.0] for i in 1:particles]
     end
 end
 
 @time simulation()
 
 fig2 = figure(2)
-plot([i for i in 1:SimulationSystem.steps], Results.temperatures)
+println(temperatures)
+plot([i for i in 1:steps], temperatures)
 
 # function plotSimulation()
 #     global finalPositions, simParticles, simSteps, simBoxSize
