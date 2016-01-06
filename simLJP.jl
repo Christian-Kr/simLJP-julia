6ac7d4c973c02aaf97b2c62cf0dec3f0786ad03f
Christian-Kr <cerebrosus@gmx.net>
2016-01-06 14:16:50 +0100

    Several small changes.

--- simLJP.jl
+++ simLJP.jl
@@ -21,6 +21,7 @@ type Model
   steps::Int64
   particles::Int64
   sideLength::Float64
+  halfSideLength::Float64
   dim::Int64
   initTemp::Float64
 
@@ -71,9 +72,10 @@ Constructor function for type Model.
 return: The contructed model object.
 """
 function Model()
-  steps::Int64 = 50000
+  steps::Int64 = 500000
   particles::Int64 = 64
   sideLength::Float64 = 6.0e-10
+  halfSideLength = sideLength / 2.0
   dim::Int64 = 2
   initTemp::Float64 = 25
   diameter::Float64 = 2.6e-10
@@ -98,9 +100,9 @@ function Model()
   forces::Array{Float64, 2} = fill(0.0, dim, particles)
   temperatures::Array{Float64, 1} = fill(0.0, steps)
 
-  return Model(steps, particles, sideLength, dim, initTemp, diameter, mass,
-               sigma, epsilon, timeStep, timeStep2, positions, velocities,
-               accelerations, forces, temperatures)
+  return Model(steps, particles, sideLength, halfSideLength, dim, initTemp,
+               diameter, mass, sigma, epsilon, timeStep, timeStep2, positions,
+               velocities, accelerations, forces, temperatures)
 end
 
 """
@@ -139,24 +141,23 @@ systemClosed: True if the system is a closed box and false if it is periodic.
 
 return:       The new position and velocity as a two component list.
 """
-function adjustPosition!(position::Array{Float64, 1}, dim::Int64,
-                        velocity::Array{Float64, 1}, sideLength::Float64,
-                        systemClosed::Bool = true)
+function adjustPosition!(position::Array{Float64, 1},
+    velocities::Array{Float64, 2}, index::Int64, halfSideLength::Float64,
+    systemClosed::Bool = true)
+
   # We suggest, that the center of the box has the coordinates [0, 0, 0].
-  halfLength::Float64 = sideLength / 2.0
-  
   if systemClosed::Bool == false
-    for i::Int64 = 1 : dim
-      if position[i] > halfLength
-        position[i] = -halfLength
-      elseif position[i] < -halfLength
-        position[i] = halfLengh
+    for i::Int64 = 1 : 2
+      if position[i] > halfSideLength
+        position[i] = -halfSideLength
+      elseif position[i] < -halfSideLength
+        position[i] = halfSideLengh
       end    
     end
   else
-    for i::Int64 = 1 : dim
-      if position[i] > halfLength || position[i] < -halfLength
-        velocity[i] *= -1
+    for i::Int64 = 1 : 2
+      if position[i] > halfSideLength || position[i] < -halfSideLength
+        velocities[i, index] *= -1
       end
     end
   end
@@ -166,37 +167,30 @@ end
 Calculate the temperature of the system based on the mass and velocity of
 every particle.
 
-mass:     Mass of the particle.
-velocity: Velocity of the particle as a vector with n-coordinates.
-
-return:   The temperature in unit Kelvin.
+temperatures: The temperatures object.
+index:        The index for temperature step.
+mass:         Mass of the particle.
+velocities:   Velocities object of all particle as a vector with n-coordinates.
 """
-function temperature(mass::Float64, velocities::Array{Float64, 2},
-                     particles::Int64)
+function temperature!(temperatures::Array{Float64, 1}, index::Int64,
+    mass::Float64, velocities::Array{Float64, 2}, particles::Int64)
+
   const kB::Float64 = 1.38064852e-23
-  
   s::Float64 = 0.0
   for i::Int64 = 1:size(velocities, 1), j::Int64 = 1:size(velocities, 2)
     s += velocities[i, j]^2
   end
 
-  return s * mass / (kB * 3.0 * (particles - 1))
+  temperatures[index] =  s * mass / (kB * 3.0 * (particles - 1))
 end
 
 "Main simulation function for running the system."
 function simulation(m::Model)
-  positionA::Array{Float64, 1} = fill(0.0, m.dim)
-  positionB::Array{Float64, 1} = fill(0.0, m.dim)
-  velocity::Array{Float64, 1} = fill(0.0, m.dim)
-  acceleration::Array{Float64, 1} = fill(0.0, m.dim)
   position::Array{Float64, 1} = fill(0.0, m.dim)
 
   # Running main loop
   for i::Int64 = 2 : m.steps
     for j::Int64 = 1 : m.particles
-      positionA[1] = m.positions[1, j, i - 1]
-      positionA[2] = m.positions[2, j, i - 1]
-      
       # Update forces
       for k::Int64 = j + 1 : m.particles
         calculateLJP!(m.forces, m.positions, j, k, i - 1, m.epsilon, m.sigma)
@@ -204,17 +198,13 @@ function simulation(m::Model)
 
       # Update particle position followed by a correction of particle position
       # and velocitiy.
-      velocity[1] = m.velocities[1, j]
-      velocity[2] = m.velocities[2, j]
-      acceleration[1] = m.accelerations[1, j]
-      acceleration[2] = m.accelerations[2, j]
-      position = positionA + velocity * m.timeStep + 0.5 * acceleration *
-                 m.timeStep2
-      adjustPosition!(position, m.dim, velocity, m.sideLength)
+      position[1] = m.positions[1, j, i - 1] + m.velocities[1, j] *
+        m.timeStep + 0.5 * m.accelerations[1, j] * m.timeStep2
+      position[2] = m.positions[2, j, i - 1] + m.velocities[2, j] *
+        m.timeStep + 0.5 * m.accelerations[2, j] * m.timeStep2
+      adjustPosition!(position, m.velocities, j, m.halfSideLength)
       m.positions[1, j, i] = position[1]
       m.positions[2, j, i] = position[2]
-      m.velocities[1, j] = velocity[1]
-      m.velocities[2, j] = velocity[2]
 
       # Update accelerations
       m.accelerations[1, j] = m.forces[1, j] / m.mass
@@ -226,7 +216,7 @@ function simulation(m::Model)
     end
     
     # Calculate and save temperature for every step.
-    m.temperatures[i] = temperature(m.mass, m.velocities, m.particles)
+    temperature!(m.temperatures, i, m.mass, m.velocities, m.particles)
     fill!(m.forces, 0.0)
   end
 end
