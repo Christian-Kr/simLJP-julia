8b82f54348afc30c41b781db255a5c5bd6e7307b
Christian-Kr <cerebrosus@gmx.net>
2016-01-06 13:00:56 +0100

    Move variables to Model.

--- simLJP.jl
+++ simLJP.jl
@@ -6,7 +6,7 @@ using PyCall
 @pyimport matplotlib.animation as animation
 
 using Distributions
-using ProfileView
+#using ProfileView
 
 ################################################################################
 # Global variables
@@ -34,10 +34,36 @@ type Model
 
   timeStep::Float64
   timeStep2::Float64
+
+  positions::Array{Float64, 3}
+  velocities::Array{Float64, 2}
+ 
+  accelerations::Array{Float64, 2}
+  forces::Array{Float64, 2}
+  temperatures::Array{Float64, 1}
 end
 
 ################################################################################
 # Functions
+
+function initPositions!(positions::Array{Float64, 3}, dim::Int64,
+    sideLength::Float64, particles::Int64)
+
+  numPartSide::Float64 = 64.0^(1.0 / dim)
+  distance::Float64 = sideLength / (numPartSide + 1.0)
+  multX::Float64 = 0.0
+  multY::Float64 = 0.0
+
+  for i::Int64 = 1 : particles
+    multX = divrem(i, numPartSide)[2]
+    multY = divrem(i - 1, numPartSide)[1]
+    
+    if multX == 0 multX = numPartSide end
+
+    positions[1, i, 1] = -(sideLength / 2.0) + distance * multX
+    positions[2, i, 1] = -(sideLength / 2.0) + distance * (multY + 1.0)
+  end
+end
 
 """
 Constructor function for type Model.
@@ -45,7 +71,7 @@ Constructor function for type Model.
 return: The contructed model object.
 """
 function Model()
-  steps::Int64 = 1
+  steps::Int64 = 5000
   particles::Int64 = 64
   sideLength::Float64 = 6.0e-10
   dim::Int64 = 2
@@ -57,8 +83,24 @@ function Model()
   timeStep::Float64 = 0.0001 * sigma * sqrt(mass / epsilon)
   timeStep2::Float64 = timeStep^2
 
+  # Init the initial positions.
+  positions::Array{Float64, 3} = fill(0.0, dim, particles, steps)
+  initPositions!(positions, dim, sideLength, particles)
+
+  # Create the velocity array and init the coordinates to the normal
+  # distribution.
+  dist = Normal(0.0, initTemp^(0.5))
+  velocities::Array{Float64, 2} = rand(dist, dim, particles)
+
+  # Create the accelerations, temperatures and forces array, which are zero at
+  # the beginning.
+  accelerations::Array{Float64, 2} = fill(0.0, dim, particles)
+  forces::Array{Float64, 2} = fill(0.0, dim, particles)
+  temperatures::Array{Float64, 1} = fill(0.0, steps)
+
   return Model(steps, particles, sideLength, dim, initTemp, diameter, mass,
-               sigma, epsilon, timeStep, timeStep2)
+               sigma, epsilon, timeStep, timeStep2, positions, velocities,
+               accelerations, forces, temperatures)
 end
 
 """
@@ -90,7 +132,7 @@ systemClosed: True if the system is a closed box and false if it is periodic.
 
 return:       The new position and velocity as a two component list.
 """
-function adjustPosition(position::Array{Float64, 1}, dim::Int64,
+function adjustPosition!(position::Array{Float64, 1}, dim::Int64,
                         velocity::Array{Float64, 1}, sideLength::Float64,
                         systemClosed::Bool = true)
   # We suggest, that the center of the box has the coordinates [0, 0, 0].
@@ -111,8 +153,6 @@ function adjustPosition(position::Array{Float64, 1}, dim::Int64,
       end
     end
   end
-
-  return (position, velocity)
 end
 
 """
@@ -137,102 +177,71 @@ function temperature(mass::Float64, velocities::Array{Float64, 2},
 end
 
 "Main simulation function for running the system."
-function simulation()
-  m::Model = Model()
-  
-  # Create the positions array and init the start positions.
-  positions::Array{Float64, 3} = fill(0.0, m.dim, m.particles, m.steps)
-
-  numPartSide::Float64 = 64.0^(1.0 / m.dim)
-  distance::Float64 = m.sideLength / (numPartSide + 1.0)
-  multX::Float64 = 0.0
-  multY::Float64 = 0.0
-  for i::Int64 = 1 : m.particles
-    multX = divrem(i, numPartSide)[2]
-    multY = divrem(i - 1, numPartSide)[1]
-    
-    if multX == 0 multX = numPartSide end
-
-    positions[1, i, 1] = -(m.sideLength / 2.0) + distance * multX
-    positions[2, i, 1] = -(m.sideLength / 2.0) + distance * (multY + 1.0)
-  end
-
-  # Create the velocity array and init the coordinates to the normal
-  # distribution.
-  dist = Normal(0.0, m.initTemp^(0.5))
-  velocities::Array{Float64, 2} = rand(dist, m.dim, m.particles)
-
-  # Create the accelerations, temperatures and forces array, which are zero at
-  # the beginning.
-  accelerations::Array{Float64, 2} = fill(0.0, m.dim, m.particles)
-  forces::Array{Float64, 2} = fill(0.0, m.dim, m.particles)
-  temperatures::Array{Float64, 1} = fill(0.0, m.steps)
-
+function simulation(m::Model)
   positionA::Array{Float64, 1} = fill(0.0, m.dim)
   positionB::Array{Float64, 1} = fill(0.0, m.dim)
   velocity::Array{Float64, 1} = fill(0.0, m.dim)
   acceleration::Array{Float64, 1} = fill(0.0, m.dim)
-  result::Tuple{Array{Float64, 1}, Array{Float64, 1}} = ([0, 0], [0, 0])
+  position::Array{Float64, 1} = fill(0.0, m.dim)
 
   # Running main loop
   for i::Int64 = 2 : m.steps
     for j::Int64 = 1 : m.particles
-      positionA[1] = positions[1, j, i - 1]
-      positionA[2] = positions[2, j, i - 1]
+      positionA[1] = m.positions[1, j, i - 1]
+      positionA[2] = m.positions[2, j, i - 1]
       
       # Update forces
       for k::Int64 = j + 1 : m.particles
-        positionB[1] = positions[1, k, i - 1]
-        positionB[2] = positions[2, k, i - 1]
+        positionB[1] = m.positions[1, k, i - 1]
+        positionB[2] = m.positions[2, k, i - 1]
         
         #forces[:, j] += calculateLJP(positionA, positionB, m.epsilon, m.sigma)
         force = calculateLJP(positionA, positionB, m.epsilon, m.sigma)
-        forces[1, j] += force[1]
-        forces[2, j] += force[2]
-        forces[1, k] -= force[1]
-        forces[2, k] -= force[2]
+        m.forces[1, j] += force[1]
+        m.forces[2, j] += force[2]
+        m.forces[1, k] -= force[1]
+        m.forces[2, k] -= force[2]
       end
 
       # Update particle position followed by a correction of particle position
       # and velocitiy.
-      velocity[1] = velocities[1, j]
-      velocity[2] = velocities[2, j]
-      acceleration[1] = accelerations[1, j]
-      acceleration[2] = accelerations[2, j]
-      result = adjustPosition(positionA + velocity * m.timeStep + 0.5 *
-                              acceleration * m.timeStep2, m.dim,
-                              velocity, m.sideLength)
-      positions[1, j, i] = result[1][1]
-      positions[2, j, i] = result[1][2]
-      velocities[1, j] = result[2][1]
-      velocities[2, j] = result[2][2]
+      velocity[1] = m.velocities[1, j]
+      velocity[2] = m.velocities[2, j]
+      acceleration[1] = m.accelerations[1, j]
+      acceleration[2] = m.accelerations[2, j]
+      position = positionA + velocity * m.timeStep + 0.5 * acceleration *
+                 m.timeStep2
+      adjustPosition!(position, m.dim, velocity, m.sideLength)
+      m.positions[1, j, i] = position[1]
+      m.positions[2, j, i] = position[2]
+      m.velocities[1, j] = velocity[1]
+      m.velocities[2, j] = velocity[2]
 
       # Update accelerations
-      accelerations[1, j] = forces[1, j] / m.mass
-      accelerations[2, j] = forces[2, j] / m.mass
+      m.accelerations[1, j] = m.forces[1, j] / m.mass
+      m.accelerations[2, j] = m.forces[2, j] / m.mass
 
       # Update velocities
-      velocities[1, j] = velocities[1, j] + accelerations[1, j] * m.timeStep
-      velocities[2, j] = velocities[2, j] + accelerations[2, j] * m.timeStep
+      m.velocities[1, j] = m.velocities[1, j] + m.accelerations[1, j] * m.timeStep
+      m.velocities[2, j] = m.velocities[2, j] + m.accelerations[2, j] * m.timeStep
     end
     
     # Calculate and save temperature for every step.
-    temperatures[i] = temperature(m.mass, velocities, m.particles)
-    fill!(forces, 0.0)
+    m.temperatures[i] = temperature(m.mass, m.velocities, m.particles)
+    fill!(m.forces, 0.0)
   end
-  
-  return (positions, temperatures)
 end
 
 #@code_warntype simulation()
-#@time result = simulation()
+m = Model()
+@time result = simulation(m)
 #@profile result = simulation()
 
 function showAnimationPlot()
   positions = result[1]
-  plots = [pyplot.plot([positions[1, i, j] for i in 1:size(positions, 2)],
-                [positions[2, i, j] for i in 1:size(positions, 2)], "ro")
-           for j in 1:size(positions, 3)]
+  plots = [pyplot.plot([m.positions[1, i, j] for i in 1:size(m.positions, 2)],
+                [m.positions[2, i, j] for i in 1:size(m.positions, 2)], "ro")
+           for j in 1:size(m.positions, 3)]
   
   pyplot.axis([-6e-10, 6e-10, -6e-10, 6e-10])
   fig1 = pyplot.figure(1)
@@ -242,7 +251,7 @@ end
 
 function showTemperaturePlot()
   pyplot.title("Molecular Dynamics Simulation", fontsize = 14)
-  pyplot.plot([i for i in 1:size(result[2], 1)], result[2])
+  pyplot.plot([i for i in 1:m.steps], m.temperatures)
   pyplot.xlabel("Timesteps")
   pyplot.ylabel("Temperature [K]")
   pyplot.show()
